4ad2f4e66e95165390475a78bd2c85ff6fe2c917
Paulo <paulotechusa@protonmail.com>
2023-11-25 00:47:45 -0600

    Init commit, volume up is working!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
-use libc::size_t;
+use clap::Parser;
+use ffi_learn::Pulse;
 
-#[link(name = "snappy")]
-extern "C" {
-    fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+#[derive(Parser)]
+#[command(name = "Rust PA Mixer")]
+#[command(author = "Paulo Guimaraes <paulotechusa@proton.me>")]
+#[command(version = option_env!("CARGO_PKG_VERSION"))]
+#[command(about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    mute: bool,
+
+    #[arg(short, long)]
+    unmute: bool,
+
+    #[arg(long)]
+    server: bool,
 }
 
 fn main() {
-    let x = unsafe { snappy_max_compressed_length(100) };
-    println!("max compressed length of a 100 byte buffer: {}", x);
+    let cli = Cli::parse();
+
+    if cli.mute {
+        println!("Add code to mute.")
+    }
+
+    if cli.unmute {
+        println!("Add code to unmute.")
+    }
+
+    if cli.server {
+        let mut pulse = Pulse::connect_to_pulse().unwrap();
+        pulse.get_sink_info();
+        pulse.update_volume();
+    }
 }
