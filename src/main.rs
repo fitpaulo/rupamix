b2c25e2960b9d746b14e32f2c5ff612ad85a12fc
Paulo <paulotechusa@protonmail.com>
2023-11-25 16:35:19 -0600

    Huge huge refactor and stuff is broken as hell now.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use rupamix::Pulse;
+use rupamix::pulse_wrapper::Pulse;
 
 #[derive(Parser)]
 #[command(name = "Rust Pulse Mixer")]
@@ -7,6 +7,9 @@ use rupamix::Pulse;
 #[command(version = option_env!("CARGO_PKG_VERSION"))]
 #[command(about, long_about = None)]
 struct Cli {
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbose: u8,
+
     #[arg(short, long)]
     toggle_mute: bool,
 
@@ -20,19 +23,26 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
 
+    if cli.verbose >= 2 {
+        simple_logger::init_with_level(log::Level::Debug).unwrap();
+    } else if cli.verbose == 1 {
+        simple_logger::init_with_level(log::Level::Info).unwrap();
+    } else {
+        simple_logger::init_with_level(log::Level::Error).unwrap();
+    }
+
     if cli.toggle_mute {
-        println!("Add code to mute.")
+        log::info!("Add code to mute.");
+        log::debug!("Blah")
     }
 
     if let Some(_increment) = cli.increase {
         let mut pulse = Pulse::connect_to_pulse().unwrap();
         pulse.get_sink_info();
-        pulse.update_volume();
     }
 
     if let Some(_increment) = cli.decrease {
         let mut pulse = Pulse::connect_to_pulse().unwrap();
         pulse.get_sink_info();
-        pulse.update_volume();
     }
 }
